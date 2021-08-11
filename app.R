684e7aadbe59c909ed69ebc3366ff556a4fad934
Jomapime <jomapime12@gmail.com>
2021-08-11 18:20:31 -0500

    Configuracion menu e iconos.

--- app.R
+++ app.R
@@ -1,24 +1,34 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
 #    http://shiny.rstudio.com/
 #
-library(pacman)
 
+library(pacman)
 pacman::p_load(shiny,readr,rsconnect,dplyr,highcharter,shinythemes,shinydashboard)
 
+#source("./global.R",encoding = "utf-8")
 
-sidebar <- 
+
+
+# Define UI for application that draws a histogram
+
+
+ui <- dashboardPage(
+  dashboardHeader(title="Dashboard Example"),
   dashboardSidebar(
     width = 200,
     sidebarMenu(
       id = "Menu",
       style = "position: relative; overflow: visible;",
-      menuItem("Encuesta", tabName = "Resultados", icon = )
+      menuItem("Encuesta", tabName = "Resultados", icon = icon("stats", lib = "glyphicon"),
+               badgeColor = "green"),
+      menuItem("Market Intelligence", tabName = 'country_intel', icon = icon("align-top", lib = "glyphicon"))
     )
-  )
+  ),
+  dashboardBody()
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {}
 
 
+# Run the application 
+shinyApp(ui = ui, server = server)
